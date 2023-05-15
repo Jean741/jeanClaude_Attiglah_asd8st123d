e2313d788fb103a4cc884b306fb182ec43653ce8
jean741 <iattiglah@gmail.com>
2023-05-16 01:35:23 +0200

    feat (init): create dataBase and webServer configurations

---
+++ src/test/java/com/anywr/school/JeanClaudeAttiglahAsd8st123dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anywr.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeanClaudeAttiglahAsd8st123dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
