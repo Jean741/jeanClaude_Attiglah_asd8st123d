f09c226d1dbdf7fddf79a5e63114ca4ad0df70f6
jean741 <iattiglah@gmail.com>
2023-05-16 01:38:30 +0200

    feat (conception): create entities and add relations between them

---
+++ src/main/java/com/anywr/school/entity/Student.java
@@ -0,0 +1,46 @@
+package com.anywr.school.entity;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = PRIVATE)
+@DiscriminatorValue("std")
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student extends SchoolMember {
+
+	public Student(UUID id, String lastName, String firstName, SchoolClass studentClass) {
+		super(id, lastName, firstName);
+		this.studentClass = studentClass;
+	}
+
+	@JoinColumn(nullable = true)
+	@ManyToOne
+	SchoolClass studentClass; 
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if (! super.equals(obj)) {
+	      return false;
+	    }
+	    Student student = (Student) obj;
+	    return studentClass.equals(student.getStudentClass());
+	  }
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(studentClass);
+		return result;
+	}
+}
