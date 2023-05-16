3cd6e7d4be6b4f17be3bb7ca08585dd1812e4e5d
jean741 <iattiglah@gmail.com>
2023-05-16 21:50:10 +0200

    feat: add students filtered by Teacher fullname

--- src/main/java/com/anywr/school/repository/SchoolClassRepository.java
+++ src/main/java/com/anywr/school/repository/SchoolClassRepository.java
@@ -1,12 +1,14 @@
 package com.anywr.school.repository;
 
 import com.anywr.school.entity.SchoolClass;
+import com.anywr.school.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SchoolClassRepository extends JpaRepository<SchoolClass, UUID> {
     SchoolClass findByName(String studentClass);
-    
-	SchoolClass findByTeacher(String studentClass);
 }
--- src/main/java/com/anywr/school/repository/TeacherRepository.java
+++ src/main/java/com/anywr/school/repository/TeacherRepository.java
@@ -2,8 +2,13 @@ package com.anywr.school.repository;
 
 import com.anywr.school.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
+    @Query("from Teacher T where (concat(T.firstName,' ',T.lastName) = :fullName or concat(T.lastName,' ',T.firstName)=:fullName)")
+    List<Teacher> findByTeacherFullName(@Param("fullName")String fullName);
 }
